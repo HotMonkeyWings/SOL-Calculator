4ff8d87c221446ee18122faf4ba5d7b765c456e1
HotMonkeyWings <devsony52@gmail.com>
2022-03-02 21:08:31 +0530

    Quest completed

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -5,10 +5,103 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod calculator {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.greeting = init_message;
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 + num2;
+        Ok(())
+    }
+
+    pub fn multiply(ctx: Context<Multiplication>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 * num2;
+        Ok(())
+    }
+
+    pub fn subtract(ctx: Context<Subtraction>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 - num2;
+        Ok(())
+    }
+
+    pub fn divide(ctx: Context<Division>, num1: i64, num2: i64) -> ProgramResult {
+        let calculator = &mut ctx.accounts.calculator;
+        calculator.result = num1 / num2;
+        calculator.remainder = num1 % num2;
         Ok(())
     }
 }
 
+/**
+ * Since Context<Addition points towards the account for addition, we
+ * need to send the calculator account to the function where its called!
+ * We'll be making similiar structures for the other 3 Operations.
+ *
+ * The `mut` parameter indicates that the account will be altered. This is good for
+ * persisting changes like calculations!
+ */
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Addition<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Multiplication<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Subtraction<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+pub struct Division<'info> {
+    #[account(mut)]
+    pub calculator: Account<'info, Calculator>,
+}
+
+#[derive(Accounts)]
+/**
+ * Three accounts will be created. 
+ * 
+ * The first account has 3 parameters:
+ *      - init: Macro used to create a new account owned by the current program
+ *      - payer: The one who will pay to create the new account
+ *      - space: Space required for the application
+ * 
+ * The Signer type for the second account enforces the constraint that the `authority` account signed the transaction.
+ * 
+ * The system_program account is just system specs for the SOL blockchain in the form of an account
+ * 
+ * For signer and mut, we can use `@<custom-error>` to define custom errors.
+ * 
+ * Check https://docs.rs/anchor-lang/0.18.0/anchor_lang/derive.Accounts.html for more account types.
+ */
+pub struct Create<'info> {
+    #[account(init, payer = user, space = 8 + 64 + 64 + 64 + 64)]
+    pub calculator: Account<'info, Calculator>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+/**
+ * An account is used as a contract in Solana. This defines our calculator account.
+ * The macro #[account] tells anchor to convert struct into an account
+ *  */
+pub struct Calculator {
+    pub greeting: String,
+    pub result: i64,
+    pub remainder: i64,
+}
--- tests/calculator.js
+++ tests/calculator.js
@@ -1,15 +1,100 @@
+const assert = require('assert');
 const anchor = require('@project-serum/anchor');
+const {
+    SystemProgram
+} = anchor.web3;
 
 describe('calculator', () => {
-
     // Configure the client to use the local cluster.
-    const provider = anchor.Provider.local("http://127.0.0.1:8899")
+    const provider = anchor.Provider.local('http://127.0.0.1:8899');
     anchor.setProvider(provider);
 
-    it('Is initialized!', async () => {
-        // Add your test here.
-        const program = anchor.workspace.Calculator;
-        const tx = await program.rpc.initialize();
-        console.log("Your transaction signature", tx);
+    const calculator = anchor.web3.Keypair.generate();
+    const program = anchor.workspace.Calculator;
+
+    it('Creates a calulator', async () => {
+        /**
+         * Make Remote Procedure Calls (RPCs) to access the function.
+         * Create accounts using web3 library and passes it as parameters to the function.
+         */
+        await program.rpc.create('Hey kids, you like violence?', {
+            accounts: {
+                calculator: calculator.publicKey,
+                user: provider.wallet.publicKey,
+                systemProgram: SystemProgram.programId,
+            },
+            signers: [calculator],
+        });
+
+        // Now we grab hold of the calculator account and check if its greeting variable has been changed.
+        const account = await program.account.calculator.fetch(
+            calculator.publicKey
+        );
+        assert.ok(account.greeting === 'Hey kids, you like violence?');
+        _calculator = calculator; // Storing for future uses
+    });
+
+    it('Adds two numbers', async () => {
+        await program.rpc.add(new anchor.BN(2), new anchor.BN(3), {
+            accounts: {
+                calculator: calculator.publicKey,
+            },
+        });
+
+        const account = await program.account.calculator.fetch(
+            calculator.publicKey
+        )
+        assert.ok(account.result.eq(new anchor.BN(5)))
+        assert.ok(account.greeting === "Hey kids, you like violence?")
+    });
+
+    it('Multiplies two numbers', async () => {
+        await program.rpc.multiply(new anchor.BN(2), new anchor.BN(3), {
+            accounts: {
+                calculator: calculator.publicKey
+            }
+        })
+
+        const account = await program.account.calculator.fetch(
+            calculator.publicKey
+        )
+
+        assert.ok(account.result.eq(new anchor.BN(6)));
+        assert.ok(!account.result.eq(new anchor.BN(13)));
+        assert.ok(account.greeting === "Hey kids, you like violence?")
+    });
+
+    it('Subtracts two numbers', async () => {
+        await program.rpc.subtract(new anchor.BN(5), new anchor.BN(3), {
+            accounts: {
+                calculator: calculator.publicKey
+            }
+        })
+
+        const account = await program.account.calculator.fetch(
+            calculator.publicKey
+        )
+
+        assert.ok(account.result.eq(new anchor.BN(2)))
+        assert.ok(!account.result.eq(new anchor.BN(5)))
+        assert.ok(!account.result.eq(new anchor.BN(-2)))
+        assert.ok(account.greeting === 'Hey kids, you like violence?');
+    });
+
+    it('Divides two numbers', async () => {
+        await program.rpc.divide(new anchor.BN(7), new anchor.BN(3), {
+            accounts: {
+                calculator: calculator.publicKey
+            }
+        })
+
+        const account = await program.account.calculator.fetch(
+            calculator.publicKey
+        )
+
+        assert.ok(account.result.eq(new anchor.BN(2)))
+        assert.ok(account.remainder.eq(new anchor.BN(1)))
+        assert.ok(!account.result.eq(new anchor.BN(123)))
+        assert.ok(account.greeting === 'Hey kids, you like violence?');
     });
 });
